bdd9b7d328695fa29f00f9c34dac989d10541aef
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-04-06 16:40:11 +0300

    Add files via upload

---
+++ Z3.py
@@ -0,0 +1,61 @@
+with open('z3.txt', 'w', encoding = 'utf-8') as f:
+ f.write('2.txt\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('1\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('Тревога началась в тринадцать часов ноль две минуты.\n')
+
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('1.txt\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('8\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('Начальник полиции\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('лично позвонил в шестнадцатый участок. А спустя одну минуту тридцать секунд\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('в дежурке и других комнатах нижнего этажа раздались звонки\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('Когда Иенсен -- комиссар шестнадцатого участка -- вышел из своего\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('кабинета, звонки еще не смолкли. Иенсен был мужчина средних лет, обычного\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('сложения, с лицом плоским и невыразительным. На последней ступеньке винтовой\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('лестницы он задержался и обвел взглядом помещение дежурки. Затем поправил\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('галстук и проследовал к машине.\n')
+
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('3.txt\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('9\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('     В  это время  дня  машины текли сплошным  блестящим  потоком,  а  среди\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('потока, будто  колонны из бетона  и стекла, высились  здания. Здесь,  в мире\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('резких граней,  люди  на тротуарах  выглядели  несчастными и  неприкаянными.\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('Одеты они были хорошо, но как-то удивительно походили друг на друга и все до\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('одного спешили. Они шли нестройными  вереницами, широко разливались, завидев\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('красный  светофор или  металлический  блеск кафе-автоматов.  Они непрестанно\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('одного спешили. Они шли нестройными  вереницами, широко разливались, завидев\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('красный  светофор или  металлический  блеск кафе-автоматов.  Они непрестанно\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('озирались по сторонам и теребили портфели и сумочки.\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('     Полицейские  машины  с  включенными  сиренами  пробивались  сквозь  эту\n')
+with open('z3.txt', 'a', encoding = 'utf-8') as f:
+ f.write('толчею.\n')
+
+with open('z3.txt', 'r', encoding = 'utf-8') as f:
+ for r in f:
+     print(r.strip())
+
+
+
