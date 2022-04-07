06f6dbce79429fd54f70cbb678745270cd27903c
Alexander <alekud18@yandex.ru>
2022-04-07 11:23:31 +0300

    d

--- script.py
+++ script.py
@@ -1,11 +1,25 @@
-import json
-from pprint import pprint
 
-with open('cook_book.txt','r', encoding='cp1251') as f:
-    r = f.read()
-    print(r)
+k = ['ingredient_name','quantity','measure']
+t = ['Яйцо',2,'шт']
+k_2 = ['ingredient_name','quantity','measure']
+t_2 =['Молоко',100,'шт']
+k_3 = ['ingredient_name','quantity','measure']
+t_3 =['Помидор',2,'шт']
+t_dict = dict(zip(k,t))
+t_dict_2 = dict(zip(k_2,t_2))
+t_dict_3 = dict(zip(k_3,t_3))
+cb = [t_dict,t_dict_2,t_dict_3]
 
-with open('cb.json','w') as fr:
-     json.dump(r,fr, ensure_ascii=False, indent = 4)
+#print(t_dict)
+#print(t_dict_2)
+#print(t_dict_3)
+#print(cb_i_n)
+print(cb) 
 
-    
+
+
+#'Омлет': [
+# {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+# {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+# {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+#]
