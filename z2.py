6e8c528c8d0f34959558e6faeeff64088bcbe3df
Kudin <91375108+Alexander18101995@users.noreply.github.com>
2022-05-28 21:53:49 +0300

    Add files via upload

---
+++ z2.py
@@ -0,0 +1,25 @@
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
+def get_shop_list_by_dishes(dishes,person_count):
+   for dish in cook_book[dishes]:
+     dict_ing = {}
+     dict_ing[dish['ingredient_name']] = {'measure': dish['measure'],'quantity': dish['quantity'] * person_count}
+     print(dict_ing )
+get_shop_list_by_dishes('Утка по-пекински',2)
+    
